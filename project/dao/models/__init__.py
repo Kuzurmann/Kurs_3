7b5518cee5e36443c3396aa71e307c5a1bc7e0a4
Kuzurmann <kuzurman@inbox.ru>
2022-07-07 00:40:53 +0300

    initial commit

---
+++ project/dao/models/__init__.py
@@ -0,0 +1,11 @@
+from .genre import GenreModel
+from .director import DirectorModel
+from .movie import MovieModel
+from .user import UserModel
+
+__all__ = [
+    "GenreModel",
+    "DirectorModel",
+    "MovieModel",
+    "UserModel"
+]
